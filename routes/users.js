7ecf1a236a376e2c9adea28440f0e5bd939fae85
Burak Doğru <burakdogru@stu.aydin.edu.tr>
2023-10-24 16:24:05 +0300

    added routers

--- routes/users.js
+++ routes/users.js
@@ -2,22 +2,31 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+   console.log(req.query.name)
    res.send('users List')
 })
 
 router.get('/new', (req, res) => {
-    res.send('new user form')
+    res.render('users/new', {firstname: 'new useer'})
 })
 
 router.post('/', (req, res) => {
+    const isValid = true
+    if(isValid) {
+        users.push({name: req.body.firstname})
+        res.redirect(`/users/${users.length}`)
+    }else {
+        console.log("Error")
+        res.render('users/new', {firstname: req.body.firstname})
+    }
 
-    res.send('create user')
 
 })
 
-router.route('/:id').get((req, res) => {
+router.route('/:id')
+.get((req, res) => {
     console.log(req.user)
-    res.send(`get user with id ` + req.params.id)
+    res.send(`get user with id  ${req.params.id}  ` +  ' Name of it ' + req.user.name)
 }).put((req, res) => {
     res.send(`update user with id ` + req.params.id)
 }).delete((req, res) => {
@@ -34,7 +43,7 @@ const users = [
 ]
 
 router.param('id', (req, res, next, id) => {
-    req.user = users[id]
+    req.user = users[id-1]
     next()
 })
 
--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use(logger)
 
-app.get('/', (req, res) => {
-    console.log('hi')
-    // res.send('Hello World!')
-    res.render('index.ejs', { name: 'John'})
-})
+app.set('view engine', 'ejs')
 
 const usersRouter = require('./routes/users')
 
